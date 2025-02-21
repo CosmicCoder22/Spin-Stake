65023a37cedd47fc99c84eb5f0c20d8d36156008
CosmicCoder22 <yura0092@gmail.com>
2025-02-21 17:05:48 +0200

    added loader and modal+error page logic

--- src/App.jsx
+++ src/App.jsx
@@ -10,56 +10,67 @@ import UserInfo from "./components/userInfo/UserInfo";
 import Footer from "./components/footer/Footer";
 
 import { motion, AnimatePresence } from "framer-motion";
-import { useState } from "react";
+import { useEffect, useState } from "react";
 
 function App() {
+  const [isLoading, setIsLoading] = useState(true);
 
-    const [isTokenSaleBlinking, setIsTokenSaleBlinking] = useState(false)
-    const handleTokenSaleBlinking = () => {
-      setIsTokenSaleBlinking(true)
-      setTimeout(() => {
-        setIsTokenSaleBlinking(false)
-      }, 2000)
-    }
+  useEffect (() => {
+    const timer = setTimeout(() => {
+      setIsLoading(false)
+    }, 4000)
+    return () => clearTimeout(timer)
+  })
 
+  const [isTokenSaleBlinking, setIsTokenSaleBlinking] = useState(false);
+  const handleTokenSaleBlinking = () => {
+    setIsTokenSaleBlinking(true);
+    setTimeout(() => {
+      setIsTokenSaleBlinking(false);
+    }, 2000);
+  };
 
-    const [isSpinBlinking, setIsSpinBlinking] = useState(false)
-    const handleSpinBlinking = () => {
-      setIsSpinBlinking(true)
-      setTimeout(() => {
-        setIsSpinBlinking(false)
-      }, 2000)
-    }
-    const [isTokenomicsBlinking, setIsTokenomicsBlinking] = useState(false)
-    const handleTokenomicsBlinking = () => {
-      setIsTokenomicsBlinking(true)
-      setTimeout(() => {
-        setIsTokenomicsBlinking(false)
-      }, 2000)
-    }
-    const [isSocialsBlinking, setIsSocialsBlinking] = useState(false)
-    const handleSocialsBlinking = () => {
-      setIsSocialsBlinking(true)
-      setTimeout(() => {
-        setIsSocialsBlinking(false)
-      }, 2000)
-    }
-    
+  const [isSpinBlinking, setIsSpinBlinking] = useState(false);
+  const handleSpinBlinking = () => {
+    setIsSpinBlinking(true);
+    setTimeout(() => {
+      setIsSpinBlinking(false);
+    }, 2000);
+  };
+  const [isTokenomicsBlinking, setIsTokenomicsBlinking] = useState(false);
+  const handleTokenomicsBlinking = () => {
+    setIsTokenomicsBlinking(true);
+    setTimeout(() => {
+      setIsTokenomicsBlinking(false);
+    }, 2000);
+  };
+  const [isSocialsBlinking, setIsSocialsBlinking] = useState(false);
+  const handleSocialsBlinking = () => {
+    setIsSocialsBlinking(true);
+    setTimeout(() => {
+      setIsSocialsBlinking(false);
+    }, 2000);
+  };
 
   return (
     <div className="body">
       <AnimatePresence>
-        {/* <Loader/> */}
-        <motion.div
-          className="content">
-          <Header handleTokenSaleBlinking={handleTokenSaleBlinking} handleSpinBlinking={handleSpinBlinking} handleTokenomicsBlinking={handleTokenomicsBlinking} handleSocialsBlinking={handleSocialsBlinking}/>
-          <TokenSale isTokenSaleBlinking={isTokenSaleBlinking} />
-          <UserInfo />
-          <SpinContainer isSpinBlinking={isSpinBlinking}/>
-          <AboutUs />
-          <Footer isSocialsBlinking={isSocialsBlinking}/>
-          <Tokenomics isTokenomicsBlinking={isTokenomicsBlinking}/>
-        </motion.div>
+        
+        {isLoading ? (<Loader />):
+        (<motion.div className="content">
+            <Header
+              handleTokenSaleBlinking={handleTokenSaleBlinking}
+              handleSpinBlinking={handleSpinBlinking}
+              handleTokenomicsBlinking={handleTokenomicsBlinking}
+              handleSocialsBlinking={handleSocialsBlinking}
+            />
+            <TokenSale isTokenSaleBlinking={isTokenSaleBlinking} />
+            <UserInfo />
+            <SpinContainer isSpinBlinking={isSpinBlinking} />
+            <AboutUs />
+            <Footer isSocialsBlinking={isSocialsBlinking} />
+            <Tokenomics isTokenomicsBlinking={isTokenomicsBlinking} />
+          </motion.div>)}
       </AnimatePresence>
     </div>
   );
--- src/components/loader/Loader.jsx
+++ src/components/loader/Loader.jsx
@@ -1,15 +1,14 @@
 import React from "react";
 import "./loader.css";
+
+import loadingLogo from '../../assets/logo.gif'
 import { motion, AnimatePresence } from "framer-motion";
 
 function Loader() {
   return (
     <AnimatePresence>
-        <motion.div
-          className="loaderBody"
-
-        >
-          <h1>Spin Stake</h1>
+        <motion.div className="loaderBody" initial={{ opacity: 1 }} animate={{ opacity: 0 }} transition={{ duration: 2, delay: 2}}>
+          <p>Spin Stake</p><img src={loadingLogo} alt="" />
         </motion.div>
     </AnimatePresence>
   );
